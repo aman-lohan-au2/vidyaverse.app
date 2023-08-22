4ee88577ee5f90c96320a2cb9aa12b716ecaa65a
Rupesh <rupesh.kumar@ilogsolution.com>
2023-08-23 00:57:19 +0530

    Roadmap Responsive Done

--- src/AboutUS/About.js
+++ src/AboutUS/About.js
@@ -24,9 +24,9 @@ const About = () => {
             <img className="about-video w-100 h-100" src={lady} />
           </div>
         </div>
-        <div className="about-roadmap">
+        <div className="about-roadmap ">
           <h1 className="about-roadmap-title text-center mt-5">Progressing from <span className="text-color">0</span> to <span className="text-color">0.1</span></h1>
-          <div className="roadmap m-auto d-flex">
+          <div className="roadmap m-auto">
             <div className="roadmap-container road-lft-div">
               <div className="roadmap-portion portion-lft1 portion1">
                 <button className="roadmap-portion-btn">October 21</button>
@@ -64,15 +64,50 @@ const About = () => {
               </div>
             </div>
           </div>
+          {/* dvdfvdf */}
+          <div className="phone-roadmap m-auto ">
+            <svg xmlns="http://www.w3.org/2000/svg" width="6" viewBox="0 0 6 958" fill="none">
+              <path d="M3 0.113249L0.113249 3L3 5.88675L5.88675 3L3 0.113249ZM2.99996 957.209L5.88671 954.322L2.99996 951.435L0.113207 954.322L2.99996 957.209ZM2.5 3L2.49996 954.322L3.49996 954.322L3.5 3L2.5 3Z" fill="#2D2D2D" />
+            </svg>
+            <div className="phone-roadmap-container">
+              <div className="roadmap-portion portion-rgt portion1">
+                <button className="roadmap-portion-btn">1 October 21</button>
+                <h3 className="roadmap-portion-title rgt-title"><div className="roadmap-indicator rgt-ind"><div className="roadmap-inner-indicator"></div></div>
+                  Vidhyaverse Begins</h3>
+                <p className="roadmap-portion-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.</p>
+              </div>
+              <div className="roadmap-portion portion-rgt ">
+                <button className="roadmap-portion-btn">2 October 21</button>
+                <h3 className="roadmap-portion-title rgt-title"><div className="roadmap-indicator rgt-ind"><div className="roadmap-inner-indicator"></div></div>
+                  Vidhyaverse Begins </h3>
+                <p className="roadmap-portion-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.</p>
+              </div>
+              <div className="roadmap-portion portion-rgt ">
+                <button className="roadmap-portion-btn">3 October 21</button>
+                <h3 className="roadmap-portion-title rgt-title">
+                  <div className="roadmap-indicator rgt-ind"><div className="roadmap-inner-indicator"></div></div>
+                  Vidhyaverse Begins</h3>
+                <p className="roadmap-portion-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.</p>
+              </div>
+              <div className="roadmap-portion portion-rgt">
+                <button className="roadmap-portion-btn ">4 October 21</button>
+                <h3 className="roadmap-portion-title rgt-title">
+                  <div className="roadmap-indicator rgt-ind"><div className="roadmap-inner-indicator"></div></div>
+                  Vidhyaverse Begins</h3>
+                <p className="roadmap-portion-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.</p>
+              </div>
+
+            </div>
+          </div>
         </div>
       </section>
       {/* ------------ Carousel Secion ----------------- */}
-      <section className="carousel-section">
+      <section className="carousel-section ">
         <h3 className="about-carousel-title text-center">We are Trusted by the best!</h3>
         <Carousels />
       </section>
       {/* ------------- Core Value Section -------------- */}
-      <section className="core-section-section my-5">
+      <section className="core-section-section my-5 ">
         <h1 className="text-center core-value-title mb-5">Our <span className="core-value-subtitle">CoreValues</span></h1>
         <div className="core-value-row m-auto">
           <div class="card core-section-card">
